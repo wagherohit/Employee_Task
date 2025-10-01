a3ce601f720a635dab0e83182888b903f0897b0c
wagherohit <rohitwaghe45@gmail.com>
2025-10-01 11:33:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require ('express')
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+
+
+})
+
+app.post('/saveForm',(req,res)=>{
+    console.log(req.body)
+    const obj = {data:req.body}
+    res.render('about.ejs',obj)
+})
+
+const PORT =3000
+app.listen(PORT,()=>{
+    console.log(`server is running ${PORT}`)
+})
